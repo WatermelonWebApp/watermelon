702961517e0f07e5f6bb12e47e27493f28a2f568
AntonReus <85575377+WatermelonWebApp@users.noreply.github.com>
2021-06-08 14:11:28 -0500

    a

--- js/init.js
+++ js/init.js
@@ -2,10 +2,10 @@
  * configuración en tu servidor de Firebase. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDN0DXykdXzlVGDe1U2WWp6rZQl2psCLdI",
-    authDomain: "rmrbdx1.firebaseapp.com",
-    projectId: "rmrbdx1",
-    storageBucket: "rmrbdx1.appspot.com",
-    messagingSenderId: "542459306880",
-    appId: "1:542459306880:web:6e64de1f33602cfc579765"
+    apiKey: "AIzaSyBcPBZKoRGO3Y1ntd34QQPwSJ2yrtE4gvA",
+    authDomain: "watermelon-webapp.firebaseapp.com",
+    projectId: "watermelon-webapp",
+    storageBucket: "watermelon-webapp.appspot.com",
+    messagingSenderId: "409265047068",
+    appId: "1:409265047068:web:5834fd2f3a24e6205341d9"
     });
