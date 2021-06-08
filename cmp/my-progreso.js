7c6486cfa8cfbd46785f2d2a5522587988b4099d
WatermelonWebApp <85575377+WatermelonWebApp@users.noreply.github.com>
2021-06-08 13:45:34 -0500

    Delete cmp directory

--- cmp/my-progreso.js
+++
@@ -1,8 +0,0 @@
-class MyProgreso extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = /* html */
-            `<progress max="100">Cargando...</progress>`;
-    }
-}
-
-customElements.define("my-progreso", MyProgreso);
